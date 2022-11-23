38340c81675a5ddb04e885b1cb866b8300f66de1
abdelemjidessaid <118571769+abdelemjidessaid@users.noreply.github.com>
2022-11-23 14:40:26 +0100

    Update GetStarted.java

--- FacebookBruteForce/src/com/mejidprojects/facebruteforce/GetStarted.java
+++ FacebookBruteForce/src/com/mejidprojects/facebruteforce/GetStarted.java
@@ -2,15 +2,12 @@ package com.mejidprojects.facebruteforce;
 
 public class GetStarted {
 
-    public static void main (String[] args) {
+   public static void main (String[] args) {
         FaceBruteForce faceBruteForce = new FaceBruteForce(
-                "hanaa.essaid.9",
-                "D:\\MY_JAVA_PROJECTS\\FacebookBruteForce\\src\\com\\mejidprojects\\facebruteforce\\hanaa.txt",
-                "C://webdrivers//chromedriver.exe"
+                "user.example.15", // username, id, email, number phone
+                "D://MyFiles//WordLists//user_15.txt", // wordlist path
+                "C://webdrivers//chromedriver.exe" // chromedriver path
         );
         faceBruteForce.startAttack();
-
-        // check again and continue
-        // error of username occurred when -> reloading the page
     }
 }
